389fc7a96307cc26cdc0f573cc3f601ebcc582ca
Keyris <KeyrisXdSnow>
2020-11-07 19:07:41 +0300

    add IntGenerator

---
+++ IntGenerator/IntGenerator.cs
@@ -0,0 +1,23 @@
+﻿﻿
+using System;
+using Faker.Generator;
+
+namespace IntGenerator
+{
+    public class IntGenerator : Generator
+    {
+        public IntGenerator()
+        {
+            
+        }
+        public override object Generate(GeneratorContext context)
+        {
+            return Random.Next(0, int.MaxValue);
+        }
+
+        public override string GetType()
+        {
+            return typeof(int).ToString();
+        }
+    }
+}
